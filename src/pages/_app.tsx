1ec012420d8474d1b7533e6b0e9c34e6ff31798b
Insun <inseonyu7@gmail.com>
2022-05-18 11:26:54 +0900

    feat: add NavBar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,36 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
+import Head from "next/head"
+import { Web3ReactProvider } from "@web3-react/core"
+import { ThemeProvider } from '@mui/material/styles'
+import theme from "../styles/theme"
+import NavBar from "../components/NavBar"
+import { providers } from "ethers"
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  function getLibrary(provider: any) {
+    return new providers.Web3Provider(provider)
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Semaphore NFT group</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <Web3ReactProvider getLibrary={(provider) => getLibrary(provider)}>
+        <ThemeProvider theme={theme}>
+          <NavBar />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </Web3ReactProvider>
+    </>
+  )
 }
 
 export default MyApp
