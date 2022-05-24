378a04d3c435b21a2248cbf0ec37b3a699fcb04f
dvlprsh <dvlprsh103@gmail.com>
2022-05-24 14:29:57 +0900

    fix: add db connection

--- src/controllers/groups.ctrl.ts
+++ src/controllers/groups.ctrl.ts
@@ -1,13 +1,16 @@
 import Group from "src/models/group"
 import { NextApiRequest, NextApiResponse } from "next"
+import { connectToDatabase } from "src/utils/backend/mongodb"
 
 export const write = async (req: NextApiRequest, res: NextApiResponse) => {
+  await connectToDatabase()
   const group = new Group(req.body)
 
   await group.save()
 }
 
 export const list = async (req: NextApiRequest, res: NextApiResponse) => {
+  await connectToDatabase()
   const groups = await Group.find().exec()
   
   res.status(200).send({ data: groups })
