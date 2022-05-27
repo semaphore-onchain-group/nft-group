49c9a85dd37b0c3d4256fe49ccfe57a93794ec5f
Insun <inseonyu7@gmail.com>
2022-05-27 15:54:55 +0900

    chore: add POAP button on admin page

--- src/utils/frontend/getGroupAdmin.ts
+++ src/utils/frontend/getGroupAdmin.ts
@@ -4,10 +4,12 @@ import { GroupType } from "src/types/group"
 
 const GENERAL_ADMIN = getNextConfig().publicRuntimeConfig.generalAdminPrivateKey
 const POH_ADMIN = getNextConfig().publicRuntimeConfig.pohAdminPrivateKey
+const POAP_ADMIN = getNextConfig().publicRuntimeConfig.poapAdminPrivateKey
 
 const ADMIN_PRIVATE_KEY = {
   [GroupType.GENERAL]: GENERAL_ADMIN,
-  [GroupType.POH]: POH_ADMIN
+  [GroupType.POH]: POH_ADMIN,
+  [GroupType.POAP]: POAP_ADMIN
 } as const
 
 const provider = new providers.JsonRpcProvider(
